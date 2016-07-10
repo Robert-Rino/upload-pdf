5930c62504b74e25747369fa24c757f66b483b79
Robert-Rino <a901002666@gmail.com>
2016-07-10 16:51:19 +0800

    add models

---
+++ models/course.rb
@@ -0,0 +1,45 @@
+require 'sequel'
+require 'base64'
+require 'json'
+
+# Holds and persists an account's information
+class BaseAccount < Sequel::Model
+  plugin :single_table_inheritance, :type
+  plugin :timestamps, update_on_create: true
+  set_allowed_columns :username, :email
+
+  one_to_many :owned_folders, class: :Folder, key: :owner_id
+  many_to_many :folders,
+               join_table: :base_accounts_folders,
+               left_key: :collaborator_id, right_key: :folder_id
+
+  plugin :association_dependencies, owned_folders: :destroy
+
+  def to_json(options = {})
+    JSON({  type: type,
+            id: id,
+            username: username,
+            email: email
+          },
+         options)
+  end
+end
+
+# Regular accounts with full credentials
+class Account < BaseAccount
+  def password=(new_password)
+    new_salt = SecureDB.new_salt
+    hashed = SecureDB.hash_password(new_salt, new_password)
+    self.salt = new_salt
+    self.password_hash = hashed
+  end
+
+  def password?(try_password)
+    try_hashed = SecureDB.hash_password(salt, try_password)
+    try_hashed == password_hash
+  end
+end
+
+# SSO accounts without passwords
+class SsoAccount < BaseAccount
+end
