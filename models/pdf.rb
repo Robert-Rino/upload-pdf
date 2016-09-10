5ceec39629c382422fd9ef66e61c8f338a9f51cb
Robert-Rino <a901002666@gmail.com>
2016-09-10 11:14:57 +0800

    edit pdf model

--- models/pdf.rb
+++ models/pdf.rb
@@ -2,11 +2,15 @@ require 'sequel'
 require 'base64'
 require 'json'
 
-# Holds and persists an account's information
 class Pdf < Sequel::Model
-  plugin :timestamps, :create=>:create_at, :update=>:update_at, update_on_create: true
+  plugin :timestamps, :create=>:create_at, :update=>:update_at, update_or_create: true
+  set_allowed_columns :slide_name
 
   many_to_one :courses
+
+  def document=(doc_plaintext)
+   self.document_plain = doc_plaintext
+  end
 
   def to_json(options = {})
     JSON({
